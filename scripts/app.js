4c8fb6de6a65ad3a32b9e0565c209698c8a1a9d9
Soichiro Wada <soichirowada@ipmulticast.net>
2026-08-17 20:08:36 +0900

    message input is updated!

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,18 @@ import { Chatroom } from './chat.js';
 import { ChatUI } from './ui.js';
 
 const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+
+console.log(newChatForm)
+console.log(newChatForm.message)
+
+newChatForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const message = newChatForm.message.value.trim();
+    chatroom.addChat(message)
+    .then(() => newChatForm.reset())
+    .catch(err => console.log(err))
+});
 
 const chatUI = new ChatUI(chatList);
 const chatroom = new Chatroom('general', 'shaun');
